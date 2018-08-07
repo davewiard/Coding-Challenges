f793dc41ed1381c457266f68cd9b3d707ba08556
Dave Wiard <dave.wiard@gmail.com>
2018-08-06 19:30:09 -0700

    Added missing stuff for base project.

--- Cayuse/src/com/davewiard/ZipCodeValidator.java
+++ Cayuse/src/com/davewiard/ZipCodeValidator.java
@@ -1,4 +1,12 @@
 package com.davewiard;
 
+import java.util.regex.Pattern;
+
 public class ZipCodeValidator {
+    final static String patternString = "^[0-9]{5}(?:-[0-9]{4})?$";
+    private static Pattern pattern = Pattern.compile(patternString);;
+
+    public boolean validate(String zipCode) {
+        return pattern.matcher(zipCode).matches();
+    }
 }
