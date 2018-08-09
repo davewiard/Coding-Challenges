7fdaff2b024858500cbe160a61c3d7dcfa6ccc99
Dave Wiard <dave.wiard@gmail.com>
2018-08-08 22:44:48 -0700

    Added private constructor to prevent instantiation.
    Also added comments.

--- Cayuse/src/com/davewiard/ZipCodeValidator.java
+++ Cayuse/src/com/davewiard/ZipCodeValidator.java
@@ -2,9 +2,24 @@ package com.davewiard;
 
 import java.util.regex.Pattern;
 
+/**
+ * ZipCodeValidator is for checking whether a given string represents a validly formatted U.S. ZIP code or ZIP+4.
+ * This class is marked final to prevent subclassing. There is nothing to extend and the class cannot be instantiated
+ * anyway.
+ */
 final class ZipCodeValidator {
     private final static String patternString = "^[0-9]{5}(?:-[0-9]{4})?$";
     private final static Pattern pattern = Pattern.compile(patternString);
+
+
+    /**
+     * Prevent instantiation of this class. There is no need for it. Access isValidZipCode() method directly through a
+     * class reference:
+     *
+     *     ZipCodeValidator.isValidZipCode(zipCode);
+     */
+    private ZipCodeValidator() {}
+
 
     /**
      * Validates if the input string contains a value formatted as a U.S. ZIP
