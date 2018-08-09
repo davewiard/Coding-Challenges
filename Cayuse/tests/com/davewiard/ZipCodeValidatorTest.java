e18c652b6c0379ce2dec029e50937f364cc2f76b
Dave Wiard <dave.wiard@gmail.com>
2018-08-08 22:42:19 -0700

    Make all methods package private instead of public.

--- Cayuse/tests/com/davewiard/ZipCodeValidatorTest.java
+++ Cayuse/tests/com/davewiard/ZipCodeValidatorTest.java
@@ -8,75 +8,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ZipCodeValidatorTest {
 
-    private ZipCodeValidator zipCodeValidator;
-
     /**
-     * Before each method marked with @Test is run create a new ZipCodeValidator
-     * object to operate on.
+     * What to do before each method marked with @Test is run
      */
     @BeforeEach
-    public void setUp() {
-        zipCodeValidator = new ZipCodeValidator();
-    }
+    void setUp() { }
 
     /**
      * What to do after each method marked with @Test is run
      */
     @AfterEach
-    public void tearDown() {
-    }
+    void tearDown() { }
 
     /**
      * Tests if a 5-digits ZIP code is valid
      */
     @Test
-    public void ValidZipCodeTest1() {
-        System.out.println("ZIP code 12345 is valid: " + zipCodeValidator.isValidZipCode("12345"));
-        assertTrue(zipCodeValidator.isValidZipCode("12345"));
+    void ValidZipCodeTest1() {
+        System.out.println("ZIP code 12345 is valid: " + ZipCodeValidator.isValidZipCode("12345"));
+        assertTrue(ZipCodeValidator.isValidZipCode("12345"));
     }
 
     /**
      * Tests if a ZIP+4 is valid
      */
     @Test
-    public void ValidZipCodeTest2() {
-        System.out.println("ZIP code 12345-6789 is valid: " + zipCodeValidator.isValidZipCode("12345-6789"));
-        assertTrue(zipCodeValidator.isValidZipCode("12345-6789"));
+    void ValidZipCodeTest2() {
+        System.out.println("ZIP code 12345-6789 is valid: " + ZipCodeValidator.isValidZipCode("12345-6789"));
+        assertTrue(ZipCodeValidator.isValidZipCode("12345-6789"));
     }
 
     /**
      * Tests if an invalid ZIP containing a letter is valid
      */
     @Test
-    public void InvalidZipCodeTest1() {
-        System.out.println("ZIP code A4321 is valid: " + zipCodeValidator.isValidZipCode("A4321"));
-        assertFalse(zipCodeValidator.isValidZipCode("A4321"));
+    void InvalidZipCodeTest1() {
+        System.out.println("ZIP code A4321 is valid: " + ZipCodeValidator.isValidZipCode("A4321"));
+        assertFalse(ZipCodeValidator.isValidZipCode("A4321"));
     }
 
     /**
      * Tests if an invalid ZIP+5 is valid
      */
     @Test
-    public void InvalidZipCodeTest2() {
-        System.out.println("ZIP code 54321-06789 is valid: " + zipCodeValidator.isValidZipCode("54321-06789"));
-        assertFalse(zipCodeValidator.isValidZipCode("54321-06789"));
+    void InvalidZipCodeTest2() {
+        System.out.println("ZIP code 54321-06789 is valid: " + ZipCodeValidator.isValidZipCode("54321-06789"));
+        assertFalse(ZipCodeValidator.isValidZipCode("54321-06789"));
     }
 
     /**
      * Tests if an invalid ZIP+4 containing a letter in the +4 is valid
      */
     @Test
-    public void InvalidZipCodeTest3() {
-        System.out.println("ZIP code 12345-678d is valid: " + zipCodeValidator.isValidZipCode("12345-678d"));
-        assertFalse(zipCodeValidator.isValidZipCode("12345-678d"));
+    void InvalidZipCodeTest3() {
+        System.out.println("ZIP code 12345-678d is valid: " + ZipCodeValidator.isValidZipCode("12345-678d"));
+        assertFalse(ZipCodeValidator.isValidZipCode("12345-678d"));
     }
 
     /**
      * Tests if an invalid ZIP+4 containing only letters in the +4 is valid
      */
     @Test
-    public void InvalidZipCodeTest4() {
-        System.out.println("ZIP code 12345-abcd is valid: " + zipCodeValidator.isValidZipCode("12345-abcd"));
-        assertFalse(zipCodeValidator.isValidZipCode("12345-abcd"));
+    void InvalidZipCodeTest4() {
+        System.out.println("ZIP code 12345-abcd is valid: " + ZipCodeValidator.isValidZipCode("12345-abcd"));
+        assertFalse(ZipCodeValidator.isValidZipCode("12345-abcd"));
     }
 }
